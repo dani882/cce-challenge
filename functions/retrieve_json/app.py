f5795536a92b012abbd4908555378e75af085df7
Jesus Rivera <dani882@gmail.com>
2021-08-05 11:22:08 -0400

    moved functions folder

---
+++ functions/retrieve_json/app.py
@@ -0,0 +1,31 @@
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+def lambda_handler(event, context):
+    message = 'Hello {} {}!'.format(event['first_name'], event['last_name'])  
+    return { 
+        'message' : message
+    }
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False1
+    """
+
+    #  If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
